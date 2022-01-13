edca974bf880f6000820db3b42702633cccbb14f
root <root@ip-172-31-0-154.ec2.internal>
2022-01-13 18:03:40 +0000

    added coverage tests specifications 2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app.js')
 
 app.listen(8000, () => {
-    console.log(`Example app listening at http://localhost:8080`)
+    console.log(`Example app listening at http://localhost:8000`)
 })
 
 module.exports = app;
